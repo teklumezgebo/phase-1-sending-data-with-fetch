9e227b336cbbcfa89d92a15835c82a66dfcaf3d8
Teklu Mezgebo <mezge002@umn.edu>
2023-02-26 16:07:49 -0600

    commit tree 3f6c44921eecfc1e01ea0e1f8e47bcef81f067b3

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+       document.querySelector('body').appendChild(document.createElement('p'))
+       document.querySelector('p').innerHTML = data.id
+    })
+    .catch(function(message) {
+        document.querySelector('body').appendChild(document.createElement('p'))
+        document.querySelector('p').innerHTML = message
+    })
+ }
